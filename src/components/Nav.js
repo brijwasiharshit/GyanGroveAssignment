9600ae9f447a97cbd2f4195c94225584ffdd7f40
brijwasiharshit <110799092+brijwasiharshit@users.noreply.github.com>
2024-03-31 11:51:32 +0530

    responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Location from './Location';
 
-//this component contains the navigation menus and the location bar.
+// This component contains the navigation menus and the location bar.
 const Nav = () => {
   return (
     <div className='flex flex-col sm:flex-row justify-between my-2 text-[#989090]'>
       <div className='flex-shrink-0 sm:mr-4'>
         <Location />
       </div>
-      <div className='mt-2 sm:mt-0 justify-center ml-1 sm:ml-0'>
-        <ul className='flex flex-wrap gap-4 sm:gap-4 sm:flex-nowrap'>
-          <li className='sm:text-gray mx-1'>Live Shows</li>
-          <li className='sm:text-gray'>Streams</li>
-          <li className='sm:text-gray'>Movies</li>
-          <li className='sm:text-gray'>Plays</li>
-          <li className='sm:text-gray'>Events</li>
-          <li className='sm:text-gray'>Sports</li>
-          <li className='sm:text-gray'>Activities</li>
+      <div className='mt-2 sm:mt-0 justify-center sm:ml-0'>
+        <ul className='flex flex-wrap gap-2 sm:gap-4 sm:flex-nowrap overflow-x-auto'>
+          <li className='sm:text-gray ml-1 text-sm'>Live Shows</li>
+          <li className='sm:text-gray text-sm'>Streams</li>
+          <li className='sm:text-gray text-sm'>Movies</li>
+          <li className='sm:text-gray text-sm'>Plays</li>
+          <li className='sm:text-gray text-sm'>Events</li>
+          <li className='sm:text-gray text-sm'>Sports</li>
+          <li className='sm:text-gray text-sm'>Activities</li>
         </ul>
       </div>
       <span></span>
